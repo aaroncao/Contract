4b56486336f8444b94749bda670c3651e5f76038
wqz <111@163.com>
2012-12-05 20:17:34 +0800

    DataAccess

---
+++ ContractWeb/DataAccess/DaUserInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+using ContractWeb.Models;
+using ContractWeb.Common;
+using ND.Lib.Data.SqlHelper;
+
+namespace ContractWeb.DataAccess
+{
+    public class DaUserInfo
+    {
+        public IList<UserInfo> getUserList()
+        {
+
+            string strSql = "select * from UserInfo";
+            SqlParameter[] param = { new SqlParameter{"@userID", "sfsd"}};
+
+            IDataReader dr = null;
+            dr = SqlHelper.ExecuteReader("", CommandType.Text, strSql, param);
+            IList<UserInfo> list = DynamicBuilder<UserInfo>.ConvertToList(dr);
+
+            return list;
+        }
+    }
+}
---
+++ ContractWeb/Libs/ND.Lib.Data.SqlHelper/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// Assembly ND.Lib.Data.SqlHelper, Version 1.0.0.0
+
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Runtime.InteropServices.Guid("7c4dd688-648e-426e-9aed-d2ff9871065b")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9  2009")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Reflection.AssemblyTitle("ND.Lib.Data.SqlHelper")]
+[assembly: System.Reflection.AssemblyCompany("")]
+[assembly: System.Reflection.AssemblyProduct("ND.Lib.Data.SqlHelper")]
+
