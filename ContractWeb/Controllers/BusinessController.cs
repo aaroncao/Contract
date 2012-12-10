c932da234cfdf682de4677df569a1831f5c570b4
wqz <111@163.com>
2012-12-10 21:21:53 +0800

    影厅信息管理

---
+++ ContractWeb/Controllers/BusinessController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContractWeb.Controllers
+{
+    public class BusinessController : Controller
+    {
+        //
+        // GET: /Business/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
